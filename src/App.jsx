e3d98b2649722c16694ec9fe7536bf5def4e610e
Gian Jake Gulfan <gianjakegulfan@Gians-MacBook-Pro.local>
2026-05-31 17:05:27 +0800

    Add Vercel Analytics integration

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import IotSection from '@/sections/IotSection'
 import Contact from '@/sections/Contact'
 import ProcessSection from '@/sections/ProcessSection'
 import Services from '@/sections/Services'
-
+import { Analytics } from "@vercel/analytics/next"
 const App = () => {
   return (
     <div className="min-h-screen bg-[#1C324C] text-white">
